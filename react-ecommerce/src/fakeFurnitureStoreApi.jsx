a047d2ff79d6f6bb10996d4bac12dd6db8136d59
Beatriz_Cascais_Filipe <Beatriz95Filipe@users.noreply.github.com>
2023-07-23 12:52:25 +0100

    new fake API

---
+++ react-ecommerce/src/fakeFurnitureStoreApi.jsx
@@ -0,0 +1,44 @@
+const categoriesData = [
+    { 
+        id: 1,
+        categoryName: "Seating",
+        categoryMainText: "The main feature of any living room is always going to be the seating. Pick and choose your seating styles to build the perfect place to relax. Unwind by yourself, spend time with your family, or entertain guests, all in ultimate comfort.",
+        categoryideo1: "/src/assets/videos/product-video-seating-1.mp4",
+        categoryVideo1Slogan: "Durable enough for everyday drama",
+        categoryVideo1Text: "Each and every component of our furniture is selected for durability, from the galvanized steel latches to the precision-milled Baltic Birch frame. The fabric is inherently scratch-resistant, thanks to an incredibly tight weave, which makes it perfect for pets and their parents.",
+        subtitle1a: "Low carbon footprint",
+        subtext1a: "Fabric made from upcycled materials",
+        subtitle1b: "Sustainable wood",
+        subtext1b: "Sourced from responsibly managed forests",
+        categoryideo2: "/src/assets/videos/product-video-seating-2.mp4",
+        categoryVideo2Slogan: "Don't sweat the spills",
+        categoryVideo2Text: "Our olefin fabric is stain-resistant thanks to a deep dyeing process that colors each individual strand to the core, as opposed to other fibers that have little pores that absorb stains. You can even spray spills with a water and bleach solution (yes, we said bleach) and blot it out to clean stains.",
+        subtitle2a: "Non toxic",
+        subtext2a: "Zero chemical additives or treatments",
+        subtitle2b: "Zero PFCs",
+        subtext2b: "Fabric free of dangerous formaldehydes"
+    },
+    { 
+        id: 2,
+        categoryName: "Living Room",
+        categoryMainText: "Modern living demands more out of our spaces, or sometimes, just more space. Which is why a little storage ingenuity goes a long way. From our stylish Index wall shelving system to our versatile credenzas, we are always figuring out how to design beautiful, useful ways to keep everything in its place.",
+        categoryideo1: "/src/assets/videos/product-video-living-1.mp4",
+        categoryVideo1Slogan: "Functional, integrated features keep things organized",
+        categoryVideo1Text: "Credenzas are often the workhorse of the living room. They are used for storage and display, they need to accommodate electronics you use every night and the knick knacks you need to keep out of sight. In short, they are among the most multi-functional pieces of furniture out there, and fortunately for you, versatility happens to be our specialty.",
+        subtitle1a: "Cord management",
+        subtext1a: "Two openings allow you to place your electronics wherever works for you",
+        subtitle1b: "Sturdy, adjustable shelving",
+        subtext1b: "Our system gives you adjustable-height shelves",
+        categoryideo2: "/src/assets/videos/product-video-living-2.mp4",
+        categoryVideo2Slogan: "Contemporary design with personalized flair",
+        categoryVideo2Text: "Clean, modern silhouette that will fit peacefully into a range of interior design styles. But we take choice a step further and let you adapt the piece to your liking. From the two wood finishes, to the leg options, to the reversible doors, there is no wrong way to customize your credenza.",
+        subtitle2a: "Two leg options",
+        subtext2a: "Minimalist straight legs or mid-century hairpin legs",
+        subtitle2b: "Reversible doors",
+        subtext2b: "Flip the doors from white laminate to natural wood whenever you please"
+    },
+
+    { id: 3, categoryName: "Outdoor" },
+    { id: 4, categoryName: "Bedroom" },
+    { id: 5, categoryName: "Storage" }
+  ];
